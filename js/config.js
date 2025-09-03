c51c636a4554c5be9e3a5bd489b0255beb1ae520
Wayne Roberts <your@email.com>
2025-09-03 20:50:46 +0800

    Update backend URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: 'https://auraquant-backend.onrender.com',
-    WS_URL: 'wss://auraquant-backend.onrender.com',
+    API_URL: 'https://auraquant-backend-4.onrender.com',
+    WS_URL: 'wss://auraquant-backend-4.onrender.com',
     VERSION: '1.0.0'
 };
